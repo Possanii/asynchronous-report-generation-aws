f840ed166de2d7e82e679d448eda987e27c9ccdc
Possanii <gpossanii@outlook.com>
2025-02-02 11:19:01 -0300

    feat: Add S3 report generation functionality
    - Configure S3 bucket and IAM permissions in serverless.yml
    - Add S3 bucket name to environment variables
    - Implement report generation and upload to S3 using multipart upload
    - Increase processReport lambda timeout to 180 seconds
    - Handle errors by aborting multipart upload on failure

--- src/functions/queues/processReport.ts
+++ src/functions/queues/processReport.ts
@@ -1,42 +1,57 @@
 import { mbToBytes } from '../../utils/mbToBytes';
 import { LeadsRepository } from '../../repositories/Leads';
+import { S3MPUManager } from '../../repositories/S3MPUManager';
+import { env } from '../../config/env';
 
 const minChunkSize = mbToBytes(6);
 
 export async function handler() {
+  const fileKey = `${new Date().toISOString()}-leads.csv`;
+
   const leadsRepository = new LeadsRepository();
+  const mpu = new S3MPUManager(env.S3_REPORTS_BUCKET_NAME ?? '', fileKey);
 
-  const header = 'ID,Nome,E-mail,Cargo\n';
+  await mpu.start();
 
-  let currentChunk = header;
+  try {
+    const header = 'ID,Nome,E-mail,Cargo\n';
 
-  for await (const { Items = [] } of leadsRepository.getLeadsGenerator()) {
-    currentChunk += Items.map(
-      (item) =>
-        item.id.S +
-        ',' +
-        item.name.S +
-        ',' +
-        item.email.S +
-        ',' +
-        item.jobTitle.S +
-        '\n'
-    ).join('');
+    let currentChunk = header;
 
-    const currentChunkSize = Buffer.byteLength(currentChunk, 'utf-8');
+    for await (const { Items = [] } of leadsRepository.getLeadsGenerator()) {
+      currentChunk += Items.map(
+        (item) =>
+          item.id.S +
+          ',' +
+          item.name.S +
+          ',' +
+          item.email.S +
+          ',' +
+          item.jobTitle.S +
+          '\n'
+      ).join('');
 
-    if (currentChunkSize < minChunkSize) {
-      continue;
+      const currentChunkSize = Buffer.byteLength(currentChunk, 'utf-8');
+
+      if (currentChunkSize < minChunkSize) {
+        continue;
+      }
+
+      console.log('Reached 6MB! Sending report to S3');
+
+      await mpu.uploadPart(Buffer.from(currentChunk, 'utf-8'));
+
+      currentChunk = '';
     }
 
     if (currentChunk) {
-      console.log('Reached 6MB! Sending report to S3');
+      console.log('There are some chunks remaining! Sending report to S3');
+
+      await mpu.uploadPart(Buffer.from(currentChunk, 'utf-8'));
     }
 
-    currentChunk = '';
-  }
-
-  if (currentChunk) {
-    console.log('There are some chunks remaining! Sending report to S3');
+    await mpu.complete();
+  } catch {
+    await mpu.abort();
   }
 }
